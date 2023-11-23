5abd6557e12fbef8a6d1712fd7ded40f54beb78d
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2023-11-23 15:18:03 +0000

    add Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,12 +5,39 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="nav container">
-        <a href="" className="nav__logo">Vasyl</a>
+        <a href="" className="nav__logo">
+          Vasyl
+        </a>
         <div className="nav__menu">
           <ul className="nav__list grid">
             <li className="nav__item">
-              <a href="" className="nav__link">
-                <i className="uil uil- nav__icon"></i>
+              <a href="#home" className="nav__link">
+                <i className="uil uil-home nav__icon"></i>Home
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="uil uil-user nav__icon"></i>About
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                <i className="uil uil-file-alt nav__icon"></i>Skills
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#services" className="nav__link">
+                <i className="uil uil-briefcase-alt nav__icon"></i>Services
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#portfolio" className="nav__link">
+                <i className="uil uil-scenary nav__icon"></i>Portfolio
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="uil uil-message nav__icon"></i>Contact
               </a>
             </li>
           </ul>
