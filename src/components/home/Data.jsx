fd1bb8cbffd79b23be47bfc13bbbc041d5e0b540
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2024-01-11 11:49:18 +0000

    feature: add home, social, data components

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Data = () => {
+  return (
+    <div className="home__data">
+      <h1 className="home__title">Vasyl Zaturskyi</h1>
+      <svg></svg>
+      <h3 className="home__subtitle">Carpenter/Joiner</h3>
+      <p className="home__description">
+        I'm carpenter/joiner with more than 13 years of professional experience
+        based in London, and I'm very passionate and dedicated to my work.
+      </p>
+      <a href="#contact" className="button button--flex">
+        Say Hello
+      </a>
+
+      <svg className="send"></svg>
+    </div>
+  );
+};
+
+export default Data;
